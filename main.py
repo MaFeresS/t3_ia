d4e66dab30f05ab4b9a799ab18b4a5f89d004362
MaFeres <maferes@miuandes.cl>
2025-06-04 17:53:07 -0400

    all done

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 from sklearn.cluster import KMeans
 from sklearn.metrics import rand_score, adjusted_rand_score
-import umap
+from umap import UMAP
 import numpy as np
+import matplotlib.pyplot as plt
 
 #loading file and labels, both np.array
 file=np.load("feat_dinov2_VocPascal.npy")
@@ -14,14 +15,14 @@ with open("VocPascal/val_voc.txt","r") as labelfile:
     labels.append(i.split("\t")[1])
 labels=np.array(labels)
 
+kmeans20=KMeans(n_clusters=20)
+kmeans50=KMeans(n_clusters=50)
 
-#scaler=StandardScaler()
+comp_sizes=[16,32,64,128,256,2]
 
 #just kmeans
-kmeans20=KMeans(n_clusters=20)
 kmeans20.fit(file)
 kmeans_20=kmeans20.labels_
-kmeans50=KMeans(n_clusters=50)
 kmeans50.fit(file)
 kmeans_50=kmeans50.labels_
 #output from just kmeans
@@ -32,24 +33,71 @@ ri50=rand_score(labels,kmeans_50)
 ari50=adjusted_rand_score(labels,kmeans_50)
 print(f"ri50: {ri50}, ari50: {ari50}")
 
-#pca settup
-pca=PCA(n_components=384)
-pca.fit(file)
-pcat=pca.transform(file)
-#PCA -> kmeans
-kmeans20=KMeans(n_clusters=20)
-kmeans20.fit(pcat)
-kmeans_20=kmeans20.labels_
-kmeans50=KMeans(n_clusters=50)
-kmeans50.fit(pcat)
-kmeans_50=kmeans20.labels_
-#output from PCA -> kmeans
-ri20=rand_score(labels,kmeans_20)
-ari20=adjusted_rand_score(labels,kmeans_20)
-print(f"pca ri20: {ri20}, pca ari20: {ari20}")
-ri50=rand_score(labels,kmeans_50)
-ari50=adjusted_rand_score(labels,kmeans_50)
-print(f"pca ri50: {ri50}, pca ari50: {ari50}")
+for i in comp_sizes:
+  #PCA settup
+  scaler=StandardScaler()
+  infile=scaler.fit_transform(file)
+  pca=PCA(n_components=i)
+  pca_file=pca.fit_transform(infile)
+  #PCA -> kmeans
+
+  kmeans20.fit(pca_file)
+  kmeans_20=kmeans20.labels_
+  kmeans50.fit(pca_file)
+  kmeans_50=kmeans20.labels_
+  #output from PCA -> kmeans
+  if i!=2:
+    print(f"comp: {i}")
+    ri20=rand_score(labels,kmeans_20)
+    ari20=adjusted_rand_score(labels,kmeans_20)
+    print(f"pca ri20: {ri20}, pca ari20: {ari20}")
+    ri50=rand_score(labels,kmeans_50)
+    ari50=adjusted_rand_score(labels,kmeans_50)
+    print(f"pca ri50: {ri50}, pca ari50: {ari50}")
+  elif i==2:
+    plt.scatter(x=pca_file[:,0],y=pca_file[:,1],c=kmeans_20)
+    plt.title("PCA 2D usando k = 20")
+    plt.xlabel("Dim 1")
+    plt.ylabel("Dim 2")
+    plt.show()
+    plt.scatter(x=pca_file[:,0],y=pca_file[:,1],c=kmeans_50)
+    plt.title("PCA 2D usando k = 50")
+    plt.xlabel("Dim 1")
+    plt.ylabel("Dim 2")
+    plt.show()
+
+for i in comp_sizes:
+  #UMAP settup
+  #scaler=StandardScaler()
+  #infile=scaler.fit_transform(file)
+  umap_=UMAP(n_components=i)
+  umap_file=umap_.fit_transform(file)
+  #UMAP -> kmeans
+
+  kmeans20.fit(umap_file)
+  kmeans_20=kmeans20.labels_
+  kmeans50.fit(umap_file)
+  kmeans_50=kmeans20.labels_
+  #output from UMAP -> kmeans
+  if i!=2:
+    print(f"comp: {i}")
+    ri20=rand_score(labels,kmeans_20)
+    ari20=adjusted_rand_score(labels,kmeans_20)
+    print(f"pca ri20: {ri20}, pca ari20: {ari20}")
+    ri50=rand_score(labels,kmeans_50)
+    ari50=adjusted_rand_score(labels,kmeans_50)
+    print(f"pca ri50: {ri50}, pca ari50: {ari50}")
+  elif i==2:
+    plt.scatter(x=umap_file[:,0],y=umap_file[:,1],c=kmeans_20)
+    plt.title("UMAP 2D usando k = 20")
+    plt.xlabel("Dim 1")
+    plt.ylabel("Dim 2")
+    plt.show()
+    plt.scatter(x=umap_file[:,0],y=umap_file[:,1],c=kmeans_50)
+    plt.title("UMAP 2D usando k = 50")
+    plt.xlabel("Dim 1")
+    plt.ylabel("Dim 2")
+    plt.show()
 
 
 print(len(file))
